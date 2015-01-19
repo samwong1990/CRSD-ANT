3e8221a0a4878146f6f543811ce8ef0c1b110b4d
samwong1990 <Sam Wong>
2015-01-19 19:44:42 +0000

    Move elm port code to js/elm-ports.js

---
+++ ant-on-mobile/www/js/Adapter/elm-ports.js
@@ -0,0 +1,42 @@
+/* This is the compatibility layer between elm and CRSD.
+ * My code will be in elm-lang, and reuse CRSD code through ports (see ports in elm)
+ */
+var adapter = Elm.worker(Elm.Adapter, { input: "" });
+document.onkeydown = function(e){
+if (e.keyCode == 37) {
+        console.log("You pressed Left");
+    } else if (e.keyCode == 39) {
+        console.log("You pressed Right");
+    }
+};
+
+function triggerKeyboardEvent(el, keyCode)
+{
+  var eventObj = document.createEventObject ?
+      document.createEventObject() : document.createEvent("Events");
+
+  if(eventObj.initEvent){
+    eventObj.initEvent("keydown", true, true);
+  }
+
+  eventObj.keyCode = keyCode;
+  eventObj.which = keyCode;
+
+  el.dispatchEvent ? el.dispatchEvent(eventObj) : el.fireEvent("onkeydown", eventObj);
+
+}
+
+adapter.ports.triggerEvent.subscribe(function(eventName) {
+    switch (eventName) {
+        case "sendLeftKeyDown":
+            triggerKeyboardEvent(document, 37);
+            console.log("leftKeyDown");
+            break;
+        case "sendRightKeyDown":
+            triggerKeyboardEvent(document.body, 39);
+            console.log("rightKeyDown");
+            break;
+        default:
+            console.log("huh?");
+    }
+});
