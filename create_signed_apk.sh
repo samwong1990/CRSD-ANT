edde32f8cfa3b654d4952e3f71ae505768b74f8b
samwong1990 <Sam Wong>
2015-02-17 01:59:05 +0000

    update apk build script

--- create_signed_apk.sh
+++ create_signed_apk.sh
@@ -3,9 +3,9 @@ set -ex
 
 cd ant-on-mobile/platforms/android/ant-build
 cordova build --release android
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ant.keystore Ant-release-unsigned.apk ant
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ant.keystore CordovaApp-release-unsigned.apk ant
 rm -rf Ant-release-signed.apk
-zipalign -v 4 Ant-release-unsigned.apk Ant-release-signed.apk
-
+zipalign -v 4 CordovaApp-release-unsigned.apk Ant-release-signed.apk
+cp Ant-release-signed.apk ../../../../Ant-release-signed.apk
 echo "Make sure you have incremented the version number or Google Play Store will reject it"
 echo "The config is at CRSD-ANT/ant-on-mobile/config.xml"
